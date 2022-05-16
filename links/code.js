b4bdb4af06039e67726a0a430a1b241349e63bad
soycypher <105491076+soycypher@users.noreply.github.com>
2022-05-16 13:54:06 +0200

    eventos

--- links/code.js
+++ links/code.js
@@ -3,4 +3,9 @@
 jQuery(document).on("click", ".cards .card", function(){
     console.log(this)
     window.open(jQuery(this).attr("destino"), '_blank').focus();
+
+    gtag('event', "click", {
+        'event_label': 'link' + jQuery(this).attr("class").replace("card ", ""),
+        'event_category': 'Interaccion',
+      });
 })
