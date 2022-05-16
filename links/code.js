574c02f04a41d453fee12d3fcaedd59e0929dfd9
soycypher <105491076+soycypher@users.noreply.github.com>
2022-05-16 14:43:35 +0200

    error medicion link

--- links/code.js
+++ links/code.js
@@ -5,7 +5,7 @@ jQuery(document).on("click", ".cards .card", function(){
     window.open(jQuery(this).attr("destino"), '_blank').focus();
 
     gtag('event', "click", {
-        'event_label': 'link' + jQuery(this).attr("class").replace("card ", ""),
+        'event_label': 'link ' + jQuery(this).attr("class").replace("card ", ""),
         'event_category': 'Interaccion',
       });
 })
