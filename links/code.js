b21353c43c4085a3e6567b2d015b0e076dc4fe0f
soycypher <105491076+soycypher@users.noreply.github.com>
2022-05-18 21:41:09 +0200

    add email and change to <a>

--- links/code.js
+++ links/code.js
@@ -1,8 +1,8 @@
 
 
 jQuery(document).on("click", ".cards .card", function(){
-    console.log(this)
-    window.open(jQuery(this).attr("destino"), '_blank').focus();
+    // console.log(this)
+    // window.open(jQuery(this).attr("destino"), '_blank').focus();
 
     gtag('event', "click", {
         'event_label': 'link ' + jQuery(this).attr("class").replace("card ", ""),
